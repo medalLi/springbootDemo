c2bce8fc8a6659336486bc0bfc268786487feb91
medalLi <1712745586@qq.com>
2019-10-13 11:47:58 +0800

    2019-10-13

--- src/main/java/springboot4_mybatis/controller/DeptController.java
+++ src/main/java/springboot4_mybatis/controller/DeptController.java
@@ -1,10 +1,8 @@
 package springboot4_mybatis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springboot4_mybatis.bean.Department;
 import springboot4_mybatis.bean.Employee;
@@ -14,10 +12,10 @@ import springboot4_mybatis.mapper.EmployeeMapper;
 
 @RestController
 public class DeptController {
-    @Autowired
+//    @Autowired
     DepartmentMapper departmentMapper;
-    @Autowired
-    EmployeeMapper employeeMapper;
+   @Autowired
+   EmployeeMapper employeeMapper;
 
 
     @GetMapping("/dept/{id}")
